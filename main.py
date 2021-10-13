15c5b65d5303358036a9230f1a7d14e0247d0e39
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2021-10-13 16:03:19 +0200

    Added a collection of functions in main

--- main.py
+++ main.py
@@ -6,9 +6,37 @@ Version 1.2.0
 """
 
 
+def deal_cards():
+    pass
+
+
+def remove_cards_from_deck():
+    pass
+
+
+def calculate_score():
+    pass
+
+
+def hit():
+    pass
+
+
+def display_details():
+    pass
+
+
+def award_winnings():
+    pass
+
+
+def run():
+    pass
+
 
 def main():
-    pass
+    print('blue', '=========Welcome To Twenty Gne Game!==========')
+
 
 if __name__ == '__main__':
     main()
