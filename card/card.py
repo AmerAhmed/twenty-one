c524dcb9238e9fee685b5f10755c488cafa490a6
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2021-10-13 15:46:56 +0200

    Added Card classs and a collection of methods

--- card/card.py
+++ card/card.py
@@ -5,3 +5,23 @@ Supervisor:  Joakim Wassberg
 Version 1.2.0
 """
 
+
+class Card:
+    def __init__(self, face_value, value):
+        self.__face_value = face_value
+        self.__value = value
+
+    @property
+    def face_value(self):
+        return self.__face_value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, val):
+        self.__value = val
+
+    def __str__(self):
+        return f'{self.face_value}'
