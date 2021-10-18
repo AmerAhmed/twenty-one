01ba4a84f08ef3235a52a0fb0637a6dd71a0c628
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2021-10-18 13:30:46 +0200

    Moving all functions to run.py file and renmed run to run_game

---
+++ run.py
@@ -0,0 +1,26 @@
+def deal_cards():
+    pass
+
+
+def remove_cards_from_deck():
+    pass
+
+
+def calculate_score():
+    pass
+
+
+def hit():
+    pass
+
+
+def display_details():
+    pass
+
+
+def award_winnings():
+    pass
+
+
+def run_game():
+    pass
