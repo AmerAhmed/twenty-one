f6c24335501b8e11495f02de5e598ac2692c0a71
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2021-10-18 13:38:59 +0200

    deal_cards with parameters limit, max_cards and returns data as a result

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-def deal_cards():
-    pass
+import random
+
+
+def deal_cards(limit, max_cards):
+    cards = [random.randint(0, limit) for _ in range(0, max_cards)]
+    while True:
+        if len(set(cards)) != len(cards):
+            cards = [random.randint(0, limit) for _ in range(0, max_cards)]
+        else:
+            break
+    return cards
 
 
 def remove_cards_from_deck():
