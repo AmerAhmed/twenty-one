7ca807af00acbb1766ee486afc5842d73aad3cbf
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2021-10-19 15:20:43 +0200

    Added description to class Hand

--- hand/hand.py
+++ hand/hand.py
@@ -7,6 +7,15 @@ Version 1.2.0
 
 
 class Hand:
+    """
+    A class for creating a Hand of Cards.
+    An instance of Hand takes a list of
+    Card objects as an argument which
+    becomes the players hand for a single
+    round. The take_card  method adds a
+    new card to the hand
+    """
+
     def __init__(self, cards):
         self.__cards = cards
 
